0fb70d44326cc5cc968b460168c5f487852d78a6
st0bb3n <52521318+st0bb3n@users.noreply.github.com>
2024-04-03 02:26:31 +0800

    Update searcher.py

--- searcher.py
+++ searcher.py
@@ -1,11 +1,12 @@
 import os
 import logging
+import sys
 from openpyxl import load_workbook
 
 # Suppress openpyxl warnings
 logging.getLogger('openpyxl').setLevel(logging.ERROR)
 
-def find_string_in_workbook(directory, search_string):
+def find_string_in_workbook(directory='.', search_string=''):
     found_info = []
     for filename in os.listdir(directory):
         if filename.endswith('.xlsx'):
@@ -19,16 +20,21 @@ def find_string_in_workbook(directory, search_string):
                             found_info.append((filename, sheet, cell.coordinate))
     return found_info
 
-# Use dot `.` to represent the current directory
-directory = '.'
-# Replace 'search_string' with the string you want to search for
-search_string = 'polder'
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: python script.py <search_string> <directory>")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        search_string = sys.argv[1]
+        found_cells = find_string_in_workbook(search_string=search_string)
+    else:
+        search_string = sys.argv[1]
+        directory = sys.argv[2]
+        found_cells = find_string_in_workbook(directory=directory, search_string=search_string)
 
-found_cells = find_string_in_workbook(directory, search_string)
-
-if found_cells:
-    print(f"The search string '{search_string}' was found in the following locations:")
-    for cell_info in found_cells:
-        print(f"In workbook '{cell_info[0]}' in sheet '{cell_info[1]}' at cell '{cell_info[2]}'")
-else:
-    print(f"The search string '{search_string}' was not found in any of the files.")
+    if found_cells:
+        print(f"The search string '{search_string}' was found in the following locations:")
+        for cell_info in found_cells:
+            print(f"In workbook '{cell_info[0]}' in sheet '{cell_info[1]}' at cell '{cell_info[2]}'")
+    else:
+        print(f"The search string '{search_string}' was not found in any of the files.")
